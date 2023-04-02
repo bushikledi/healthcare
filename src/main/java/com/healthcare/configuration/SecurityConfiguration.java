5644b50669af81dc4d9606f6f3ffb1ed8ea1bfe5
Kledi <bushikledi01@gmail.com>
2023-04-02 15:16:02 +0200

    added configuration

---
+++ src/main/java/com/healthcare/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.healthcare.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+//                .requestMatchers("*").permitAll()
+                .anyRequest().permitAll();
+//                .authenticated()
+        return http.build();
+    }
+}
