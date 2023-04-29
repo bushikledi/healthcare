9cbe0abd89966685f776f17c46f1e150d9d0aeef
Kleush <kleamujo24@gmail.com>
2023-04-29 11:03:48 +0200

    Appointment

--- src/main/java/com/healthcare/model/Appointment.java
+++ src/main/java/com/healthcare/model/Appointment.java
@@ -1,4 +1,35 @@
 package com.healthcare.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="date_time")
+    private LocalDate dateTime;
+
+    @Column(name="status_id")
+    private String statusId;
+
+    @Column(name="user_id")
+    private int userId;
+
+    @Column(name="doctor_id")
+    private int doctorId;
+
+    @Column(name="billing_id")
+    private int billingId;
+
+
 }
--- src/main/java/com/healthcare/service/AppointmentService.java
+++ src/main/java/com/healthcare/service/AppointmentService.java
@@ -1,4 +1,49 @@
 package com.healthcare.service;
 
+import com.healthcare.model.Appointment;
+import com.healthcare.model.Billing;
+import com.healthcare.repository.AppointmentRepository;
+
+import java.util.List;
+
 public class AppointmentService {
+    AppointmentRepository appointmentRepository;
+
+    public AppointmentService(AppointmentRepository appointmentRepository){ this.appointmentRepository=appointmentRepository;}
+
+    public Appointment saveAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    public void deleteAppointment(Integer id) {
+        try {
+            appointmentRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't delete appointment!\n" + e.getMessage());
+        }
+    }
+
+    public Appointment updatedAppointment(Appointment updatedAppointment, Integer id) {
+        Appointment appointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("appointment not found!"));
+        appointment.setDateTime(updatedAppointment.getDateTime());
+        appointment.setUserId(updatedAppointment.getUserId());
+        appointment.setDoctorId(updatedAppointment.getDoctorId());
+        appointment.setBillingId(updatedAppointment.getBillingId());
+        appointmentRepository.save(appointment);
+        return appointment;
+    }
+
+
+    public Appointment findBillingByUserId(Integer userId) {
+        Appointment appointment = appointmentRepository.findByUserId(userId);
+        if (appointment == null) {
+            throw new RuntimeException("appointment not found for user with ID " + userId);
+        }
+        return appointment;
+    }
+
+    public List<Appointment> findAllAppointment() {
+        return appointmentRepository.findAll();
+    }
 }
