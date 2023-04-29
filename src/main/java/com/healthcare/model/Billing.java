8db5b09ea594a04afebbbb465754fa0b14d55d13
Kleush <kleamujo24@gmail.com>
2023-04-29 10:38:18 +0200

    billing

--- src/main/java/com/healthcare/model/Billing.java
+++ src/main/java/com/healthcare/model/Billing.java
@@ -1,4 +1,31 @@
 package com.healthcare.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name="billing")
+
 public class Billing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="user_id")
+    private int userId; //lidhja eshte bere ne databaze
+
+    @Column(name="amount")
+    private double amount;
+
+
+
 }
